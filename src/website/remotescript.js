873922494acff8bf1ad4247f86167e2474f55376
jogando <jogando@jogandos-MacBook-Pro.local>
2018-02-09 09:26:07 -0300

    commi

--- src/website/remotescript.js
+++ src/website/remotescript.js
@@ -15,16 +15,93 @@ RemoteClass.prototype.setControlHost = function(ref) {
     document.head.appendChild(script);
 };
 
-RemoteClass.prototype.onSdkLoad = function(ref) {
+RemoteClass.prototype.onSdkLoad = function() {
     AWS.config.region = 'us-east-1'; // Region
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     // Provide your Pool Id here
-        IdentityPoolId: 'us-east-1:XXXXX',
+        IdentityPoolId: 'us-east-1:f40ca8b1-d79a-43a9-9316-ee4ab3e76d37',
     });
 
     var lexruntime = new AWS.LexRuntime();
     var lexUserId = 'chatbot-demo' + Date.now();
     var sessionAttributes = {};
 
+}
+
+RemoteClass.prototype.pushChat = function() {
+    var self = this;
+    var wisdomText = document.getElementById('wisdom');
+    if (wisdomText && wisdomText.value && wisdomText.value.trim().length > 0) {
+
+        // disable input to show we're sending it
+        var wisdom = wisdomText.value.trim();
+        wisdomText.value = '...';
+        wisdomText.locked = true;
+
+        // send it to the Lex runtime
+        var params = {
+            botAlias: '$LATEST',
+            botName: 'BookTrip',
+            inputText: wisdom,
+            userId: lexUserId,
+            sessionAttributes: sessionAttributes
+        };
+        this.showRequest(wisdom);
+        lexruntime.postText(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+                self.showError('Error:  ' + err.message + ' (see console for details)')
+            }
+            if (data) {
+                // capture the sessionAttributes for the next cycle
+                sessionAttributes = data.sessionAttributes;
+                // show response and/or error/dialog status
+                showResponse(data);
+            }
+            // re-enable input
+            wisdomText.value = '';
+            wisdomText.locked = false;
+        });
+    }
+    // we always cancel form submission
+    return false;
+}
+
+RemoteClass.prototype.showRequest = function(daText) {
+    var conversationDiv = document.getElementById('conversation');
+    var requestPara = document.createElement("P");
+    requestPara.className = 'userRequest';
+    requestPara.appendChild(document.createTextNode(daText));
+    conversationDiv.appendChild(requestPara);
+    conversationDiv.scrollTop = conversationDiv.scrollHeight;
+}
+
+RemoteClass.prototype.showError = function(daText) {
+    var conversationDiv = document.getElementById('conversation');
+    var errorPara = document.createElement("P");
+    errorPara.className = 'lexError';
+    errorPara.appendChild(document.createTextNode(daText));
+    conversationDiv.appendChild(errorPara);
+    conversationDiv.scrollTop = conversationDiv.scrollHeight;
+}
+
+RemoteClass.prototype.showReponse = function(lexResponse) {
+    var conversationDiv = document.getElementById('conversation');
+    var responsePara = document.createElement("P");
+    responsePara.className = 'lexResponse';
+    if (lexResponse.message) {
+        responsePara.appendChild(document.createTextNode(lexResponse.message));
+        responsePara.appendChild(document.createElement('br'));
+    }
+    if (lexResponse.dialogState === 'ReadyForFulfillment') {
+        responsePara.appendChild(document.createTextNode(
+            'Ready for fulfillment'));
+        // TODO:  show slot values
+    } else {
+        responsePara.appendChild(document.createTextNode(
+            '(' + lexResponse.dialogState + ')'));
+    }
+    conversationDiv.appendChild(responsePara);
+    conversationDiv.scrollTop = conversationDiv.scrollHeight;
 }
 

87f4980a7e57841bb36a552c4be006af45ab2f88
jogando <jogando@jogandos-MacBook-Pro.local>
2018-02-09 09:15:33 -0300

    commit

--- src/website/remotescript.js
+++ src/website/remotescript.js
@@ -4,5 +4,27 @@ function RemoteClass () {
  
 RemoteClass.prototype.setControlHost = function(ref) {
     this.controlHost = ref;
-    console.log("julian");
+    
+    var script = document.createElement('script');
+    var self = this;
+    script.onload = function () {
+        self.onSdkLoad();
+    };
+    script.src = "https://sdk.amazonaws.com/js/aws-sdk-2.41.0.min.js";
+    
+    document.head.appendChild(script);
 };
+
+RemoteClass.prototype.onSdkLoad = function(ref) {
+    AWS.config.region = 'us-east-1'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    // Provide your Pool Id here
+        IdentityPoolId: 'us-east-1:XXXXX',
+    });
+
+    var lexruntime = new AWS.LexRuntime();
+    var lexUserId = 'chatbot-demo' + Date.now();
+    var sessionAttributes = {};
+
+}
+
